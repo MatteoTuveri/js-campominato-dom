1a408502874337b8a955aa9a18b86748255b37ca
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2023-10-20 01:08:21 +0200

    add counter

--- JS/script.js
+++ JS/script.js
@@ -5,13 +5,14 @@ const playground = document.getElementById('playground');
 const difficulty = document.querySelector('.form-select');
 const msg = document.querySelector('.msg');
 const buttonMsg = document.querySelector('.btn-warning');
-const text = document.querySelector('h1')
+const text = document.querySelector('h1');
+const counterMsg= document.querySelector('.counter')
 
 start.addEventListener('click', () => {
     playground.classList.remove('text-white');
     playground.innerHTML = ''
     let squareNumber;
-    let bombsNumber = 16;
+    let bombsNumber = 1;
     let counter = 0;
     if (difficulty.value === 'easy') {
         squareNumber = 100;
@@ -38,7 +39,7 @@ start.addEventListener('click', () => {
     {
         let square = document.createElement('div');
         square.classList.add('square');
-        let cb = () => {
+        square.addEventListener('click', () => {
             if (bombs.includes(i)) {
                 square.classList.add('bomb');
                 square.innerHTML = `<i class="fa-solid fa-bomb fa-shake fa-xl"></i>`
@@ -49,19 +50,18 @@ start.addEventListener('click', () => {
             else {
                 square.classList.add('active');
                 square.innerHTML = `${i}`
-                counter += 1;
+                counter += 1000;
+                counterMsg.innerHTML=`${counter}`
+                if (counter === (squareNumber - bombsNumber)*1000) {
+                    gameOver(playground);
+                    msg.classList.remove(`msg-none`);
+                    text.innerHTML = `hai vinto!`
+                }
             }
-            square.removeEventListener('click', cb)
-        };
-        square.addEventListener('click', cb);
+            square.removeEventListener('click', square);
+        });
+        square.addEventListener('click', square);
         playground.append(square);
-    }
-
-    console.log(counter);
-    if (counter === (squareNumber - bombsNumber)) {
-        gameOver(playground);
-        msg.classList.remove(`msg-none`);
-        text.innerHTML = `hai vinto!`
     }
 
 })
