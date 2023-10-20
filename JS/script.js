76cd3ee2d660ca54b034977a60b32b1d5af620e0
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2023-10-20 14:09:54 +0200

    Add block for the playground

--- JS/script.js
+++ JS/script.js
@@ -6,7 +6,7 @@ const difficulty = document.querySelector('.form-select');
 const msg = document.querySelector('.msg');
 const buttonMsg = document.querySelector('.btn-warning');
 const text = document.querySelector('h2');
-const counterMsg= document.querySelector('.counter')
+const counterMsg = document.querySelector('.counter')
 
 start.addEventListener('click', () => {
     playground.classList.remove('text-white');
@@ -24,11 +24,12 @@ start.addEventListener('click', () => {
     else if (difficulty.value === 'hard') {
         squareNumber = 49;
     }
-    else {
-        squareNumber = '100%';
-        playground.innerHTML = `Scegliere la difficoltà`
-        playground.classList.add('text-white');
+    else if (difficulty.value === 'select') {
+        squareNumber = 0;
     }
+
+    if(squareNumber!==0){
+
 
     let bombs = bombsGenerator(bombsNumber, 1, squareNumber);
     console.log(bombs);
@@ -37,22 +38,22 @@ start.addEventListener('click', () => {
     {
         let square = document.createElement('div');
         square.classList.add('square');
-        square.id=`cell-${i}`;
+        square.id = `cell-${i}`;
         square.style.width = `calc(800px / ${Math.sqrt(squareNumber)}`;
         square.style.height = `calc(800px / ${Math.sqrt(squareNumber)}`;
-        let radar = bombRadar(i,squareNumber,bombs);
+        let radar = bombRadar(i, squareNumber, bombs);
         square.addEventListener('click', bombsAndCells);
-        
+
         function bombsAndCells() {
 
             if (bombs.includes(i)) {
                 square.classList.add('bomb');
                 square.innerHTML = `<i class="fa-solid fa-bomb fa-shake fa-xl"></i>`
                 msg.classList.remove(`msg-none`);
-                gameOver(playground,bombs);
+                gameOver(playground, bombs);
                 text.innerHTML = `hai perso`;
 
-                for(let i = 1; i<=squareNumber;i++){
+                for (let i = 1; i <= squareNumber; i++) {
 
                 }
             }
@@ -60,17 +61,22 @@ start.addEventListener('click', () => {
                 square.classList.add('active');
                 square.innerHTML = `${radar}`
                 counter += 1000;
-                counterMsg.innerHTML=`${counter}`
-                if (counter === (squareNumber - bombsNumber)*1000) {
-                    gameOver(playground,bombs);
+                counterMsg.innerHTML = `${counter}`
+                if (counter === (squareNumber - bombsNumber) * 1000) {
+                    gameOver(playground, bombs);
                     msg.classList.remove(`msg-none`);
                     text.innerHTML = `hai vinto!`
                 }
             }
-            this.removeEventListener('click',bombsAndCells);
+            this.removeEventListener('click', bombsAndCells);
         }
 
         playground.append(square);
     }
 
+    }
+    else{
+        playground.innerHTML = `Scegliere la difficoltà`
+        playground.classList.add('text-white');
+    }
 })
